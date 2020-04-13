9cdbc4f53caa2beb954f7c5974eec922574669e0
Vedat Sengul <vedatsengul@gmail.com>
2020-04-13 13:40:18 +0100

    Add project files.

---
+++ CodilitySolutions/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodilitySolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BinaryGap.solution(561892));
+
+            Console.Read();
+        }
+    }
+}
