b21d5378f427dede9f240746b0769f521d84ace9
Vedat Sengul <vedatsengul@gmail.com>
2020-04-13 15:19:49 +0100

    Frog Jump

---
+++ CodilitySolutions/3.1-FrogJmp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodilitySolutions
+{
+    public static class FrogJmp
+    {
+        public static int solution(int X, int Y, int D)
+        {
+            int distance = Y - X;
+            int numberOfJumps = distance / D;
+
+            if (distance % D != 0)
+            {
+                numberOfJumps++;
+            }
+
+            return numberOfJumps;
+        }
+    }
+}
--- CodilitySolutions/Program.cs
+++ CodilitySolutions/Program.cs
@@ -8,7 +8,9 @@ namespace CodilitySolutions
         {
             //Console.WriteLine(BinaryGap.solution(561892));
             //var result = CyclicRotation.solution(new int[] { -1, -2, -3, -4, -5, -6 }, 10);
-            Console.WriteLine(OddOccurrencesInArray.solution(new int[] { 9, 3, 9, 3, 9, 7, 9 }));
+            //Console.WriteLine(OddOccurrencesInArray.solution(new int[] { 9, 3, 9, 3, 9, 7, 9 }));
+            Console.WriteLine(FrogJmp.solution(5, 105, 3));
+
             Console.Read();
         }
     }
