ceebc915e057800a13df5f1176660725a9e0b837
Vedat Sengul <vedatsengul@gmail.com>
2020-04-13 14:14:37 +0100

    Cyclic rotation

---
+++ CodilitySolutions/2.1-CyclicRotation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodilitySolutions
+{
+    public static class CyclicRotation
+    {
+        public static int[] solution(int[] A, int K)
+        {
+            if (A.Length == 0 || K == 0)
+            {
+                return A;
+            }
+
+            var result = new int[A.Length];
+
+            int shiftCount = K % A.Length;
+
+            for(int i=0;i<A.Length;i++)
+            {
+                int index = (i + shiftCount) % A.Length;
+                result[index] = A[i];
+            }
+
+            return result;
+        }
+    }
+}
--- CodilitySolutions/Program.cs
+++ CodilitySolutions/Program.cs
@@ -6,8 +6,8 @@ namespace CodilitySolutions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(BinaryGap.solution(561892));
-
+            //Console.WriteLine(BinaryGap.solution(561892));
+            var result = CyclicRotation.solution(new int[] { -1, -2, -3, -4, -5, -6 }, 10);
             Console.Read();
         }
     }
