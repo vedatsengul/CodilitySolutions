20d2165f44d8a40ad7833f8a04053cf34b384fee
Vedat Sengul <vedatsengul@gmail.com>
2020-04-14 17:02:26 +0100

    Passing cars

--- CodilitySolutions/Program.cs
+++ CodilitySolutions/Program.cs
@@ -15,7 +15,8 @@ namespace CodilitySolutions
             //Console.WriteLine(FrogRiverOne.solution(5, new int[] { 1, 3, 1, 4, 2, 3, 5, 4 }));
             //var result = MaxCounters.solution(5, new int[] { 3, 4, 4, 6, 1, 4, 4 });
             //Console.WriteLine(MissingInteger.solution(new int[] { 1, 3, 6, 4, 1, 2 }));
-            Console.WriteLine(PermCheck.solution(new int[] { 4, 1, 3 }));
+            //Console.WriteLine(PermCheck.solution(new int[] { 4, 1, 3 }));
+            Console.WriteLine(PassingCars.solution(new int[] { 1, 0, 1, 0, 0, 1, 1 }));
             Console.Read();
         }
     }
